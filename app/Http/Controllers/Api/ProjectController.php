79cede8e5248ad181de7d0b7617dedf078f2ddc3
A. L. Zailani <devpsys@gmail.com>
2024-05-10 11:37:10 +0100

    deliverables re-done

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -174,20 +174,24 @@ class ProjectController extends Controller
 
     public function deliverables($commitment_id)
     {
-        $deliverables = Deliverable::where(['commitment_id' => $commitment_id])->get();
-        $data = [];
-        foreach ($deliverables as $deliverable) {
-            $data[] = [
-                'id' => $deliverable->id,
-                'commitment_id' => $deliverable->commitment_id,
-                'deliverable' => $deliverable->deliverable,
-                'budget' => '₦' . number_format($deliverable->budget, 2),
-                'start_date' => date_format(date_create($deliverable->start_date), "d M, Y"),
-                'end_date' => date_format(date_create($deliverable->end_date), "d M, Y"),
-            ];
-        }
+        try {
+            $deliverables = Deliverable::where(['commitment_id' => $commitment_id])->get();
+            $data = [];
+            foreach ($deliverables as $deliverable) {
+                $data[] = [
+                    'id' => $deliverable->id,
+                    'commitment_id' => $deliverable->commitment_id,
+                    'deliverable' => $deliverable->deliverable,
+                    'budget' => '₦' . number_format($deliverable->budget, 2),
+                    'start_date' => date_format(date_create($deliverable->start_date), "d M, Y"),
+                    'end_date' => date_format(date_create($deliverable->end_date), "d M, Y"),
+                ];
+            }
 
-        return $data;
+            return response()->json(['success' => true, 'message' => 'Deliverables list', 'data' => $data]);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'message' => $e->getMessage(), 'data' => []]);
+        }
     }
 
     public function getUser(Request $request)
