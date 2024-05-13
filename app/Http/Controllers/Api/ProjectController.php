fde9c1f50fa23bee40dde5b450e4216924425075
A. L. Zailani <devpsys@gmail.com>
2024-05-13 15:28:13 +0100

    imported hash

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -16,6 +16,7 @@ use App\Models\UserRole;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class ProjectController extends Controller
 {
