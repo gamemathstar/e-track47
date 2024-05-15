5ceaf230260c321c0d02afd52a0a99ae6d8fc049
A. L. Zailani <devpsys@gmail.com>
2024-05-15 17:42:33 +0100

    notifications on tracking added

--- app/Http/Controllers/DeliverableController.php
+++ app/Http/Controllers/DeliverableController.php
@@ -7,8 +7,11 @@ use App\Models\Deliverable;
 use App\Models\DeliveryKpi;
 use App\Models\Kpi;
 use App\Models\KpiTarget;
+use App\Models\Notification;
 use App\Models\PerformanceTracking;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class DeliverableController extends Controller
@@ -37,7 +40,7 @@ class DeliverableController extends Controller
         return redirect()->back()->with('success', 'Deliverable created successfully');
     }
 
-    public function  storeTracking(Request $request)
+    public function storeTracking(Request $request)
     {
 //        return $request;
         $request->validate([
@@ -47,14 +50,24 @@ class DeliverableController extends Controller
 //            'id'=>'required:exists'
         ]);
         $pt = PerformanceTracking::find($request->id);
-        if($pt){
+        if ($pt) {
             $pt->delivery_department_value = $request->delivery_department_value;
             $pt->delivery_department_remark = $request->delivery_department_remark;
             $pt->confirmation_status = $request->confirmation_status;
             $pt->save();
+
+            $user = Auth::user();
+            $receiverId = $pt->kpi->deliverable->commitment->sector->sector_head_id; // ->sector_name
+            $receiver = User::find($receiverId);
+
+            $body = 'Delivery Department reviewed your submission on ' . $pt->kpi->kpi . ' ' . $pt->confirmation_status;
+            $forme = 'Your review on ' . $pt->kpi->kpi . ' has been submitted to ' . $receiver->role()->role . ' of ' . $receiver->sector()->sector_name;
+
+            Notification::make($user, $receiver, $pt, 'Tracking Reviewed', $body, 'Tracking Reviewed');
+            Notification::make($receiver, $user, $pt, 'Tracking Reviewed', $forme, 'System');
         }
 
-        return redirect()->back()->with('success', 'Delivery '.$request->confirmation_status );
+        return redirect()->back()->with('success', 'Delivery ' . $request->confirmation_status);
     }
 
     public function view(Request $request)
@@ -64,27 +77,27 @@ class DeliverableController extends Controller
         return view('pages.sector.deliverable', compact('deliverable', 'commitment'));
     }
 
-    public function kpis(Request $request,Deliverable $deliverable)
+    public function kpis(Request $request, Deliverable $deliverable)
     {
         $kpis = $deliverable->kpis()->get();
-        $year = $request->year?:2024;
+        $year = $request->year ?: 2024;
 
-        foreach ($kpis as $kpi){
-            $targt = KpiTarget::where(['year'=>$year,'kpi_id'=>$kpi->id])->first();
-            if(!$targt){
+        foreach ($kpis as $kpi) {
+            $targt = KpiTarget::where(['year' => $year, 'kpi_id' => $kpi->id])->first();
+            if (!$targt) {
                 $targt = new KpiTarget();
-                $targt->year =$year;
-                $targt->kpi_id =$kpi->id;
-                $targt->target ="";
+                $targt->year = $year;
+                $targt->kpi_id = $kpi->id;
+                $targt->target = "";
                 $targt->save();
             }
         }
-        $targets = Kpi::leftJoin("kpi_targets",function ($join)use ($year){
-            $join->on("kpi_targets.kpi_id","=","kpis.id")
-                ->on('year',"=",DB::raw($year));
+        $targets = Kpi::leftJoin("kpi_targets", function ($join) use ($year) {
+            $join->on("kpi_targets.kpi_id", "=", "kpis.id")
+                ->on('year', "=", DB::raw($year));
         })
-            ->where(['deliverable_id'=>$deliverable->id])->get();
-        return view('pages.sector.kpis', compact('deliverable', 'kpis','year','targets'));
+            ->where(['deliverable_id' => $deliverable->id])->get();
+        return view('pages.sector.kpis', compact('deliverable', 'kpis', 'year', 'targets'));
     }
 
     public function addKPI(Request $request)
--- app/Http/Controllers/KpiController.php
+++ app/Http/Controllers/KpiController.php
@@ -4,8 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Kpi;
 use App\Models\KpiTarget;
+use App\Models\Notification;
 use App\Models\PerformanceTracking;
+use App\Models\User;
+use App\Models\UserRole;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class KpiController extends Controller
 {
@@ -40,6 +44,16 @@ class KpiController extends Controller
             $tracking = PerformanceTracking::find($request->id);
         $tracking->fill($request->all());
         $tracking->save();
+
+        $user = Auth::user();
+        $receiverId = UserRole::where(['role' => 'Delivery Department', 'role_status' => 'Active'])->pluck('user_id');
+        $receiver = User::whereIn('id', $receiverId)->first();
+
+        $body = $user->role()->role . ' of ' . $user->sector()->sector_name . ' made a submission on ' . $tracking->kpi->kpi . '. It awaits your review';
+        $forme = 'Your request on ' . $tracking->kpi->kpi . ' has been submitted to Delivery Department. It is waiting for review';
+
+        Notification::make($user, $receiver, $tracking, 'Review Request', $body, 'Tracking Submitted');
+        Notification::make($receiver, $user, $tracking, 'Tracking Submitted', $forme, 'System');
 
         return back();
     }
